5602aadcc421d94f9a4759aa2fe137aa1988a58f
Adam Kiss <kiss.adam.a@gmail.com>
2020-03-28 19:01:00 +0200

    Change configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Andra Neagu Personal Website`,
+    author: `Andra Neagu`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
